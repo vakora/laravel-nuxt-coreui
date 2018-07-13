328a89007f9184387444bc8236bf831499ed02c9
Vahid Arjmand <v.arjmand66@gmail.com>
2018-07-13 01:28:57 -0700

    fresh

---
+++ client/pages/posts/create.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, dicta dolorum eligendi eos esse illum iusto pariatur placeat qui quia reprehenderit suscipit vel voluptatem. Blanditiis ipsam quae quia quis rem!</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "create"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/pages/posts/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <card :title="$t('posts')" >
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ipsum, iure nisi pariatur provident repudiandae ullam. Consequuntur, laudantium quo. Adipisci aliquam amet at culpa ea ipsam magnam officia similique ut!</p>
+        <VButton :large="true">sad</VButton>
+    </card>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+
+        }),
+        layout: 'default',
+        head () {
+            return { title: this.$t('home') }
+        },
+        middleware: 'auth' ,
+    }
+</script>
